948c183dbe4cd73a703194520fb4138f27ccb9d7
matsumoto kaoru <bc.rw0130@gmail.com>
2025-02-15 16:54:32 +0900

    ログイン画面、登録画面、商品一覧画面の追加

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>coachtech flea market</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+  @yield('css')
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <div class="header-logo">
+        <img src="{{ asset('images/logo.svg') }}" alt="ロゴ">
+      </div>
+    </div>
+  </header>
+
+  <main>
+    @yield('content')
+  </main>
+</body>
+
+</html>
