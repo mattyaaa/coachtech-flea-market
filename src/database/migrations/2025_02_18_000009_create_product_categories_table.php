a8550a0d6ce4b73b63c8b3a50c3e09c73c02065a
matsumoto kaoru <bc.rw0130@gmail.com>
2025-02-18 22:52:40 +0900

    テーブル作成

---
+++ src/database/migrations/2025_02_18_000009_create_product_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->primary(['product_id', 'category_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
