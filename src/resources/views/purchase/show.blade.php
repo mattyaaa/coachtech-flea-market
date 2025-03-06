ed8afb9ae3a89dc55dfdc05fdf0da21af78fb93d
matsumoto kaoru <bc.rw0130@gmail.com>
2025-03-06 21:55:26 +0900

    商品購入画面のCSS

--- src/resources/views/purchase/show.blade.php
+++ src/resources/views/purchase/show.blade.php
@@ -18,37 +18,49 @@
       <div class="item-info">
         <h2>{{ $item->name }}</h2>
         <p class="item-info-price">{{ $item->formatted_price }}</p>
-    </div>
-    <div class="purchase-form">
-      <h3>支払い方法</h3>
-      <div class="form-group">
-        <label for="payment_method">支払い方法</label>
-        <select name="payment_method" class="form-control" required>
-            <option value="" disabled selected>選択してください</option>
-            <option value="コンビニ支払い">コンビニ支払い</option>
-            <option value="カード支払い">カード支払い</option>
-        </select>
       </div>
-      <h3>配送先</h3>
-      <a href="/purchase/{{ $item->id }}/change-address" class="btn btn-secondary">住所を変更する</a>
-      <form action="/purchase/{{ $item->id }}" method="POST">
-        @csrf
-        <div class="form-group">
-          <p class="form-control-static">〒{{ $profile->postal_code }}</p>
-        </div>
-        <div class="form-group">
-          <p class="form-control-static">{{ $profile->address }}</p>
-        </div>
-        <div class="form-group">
-          <p class="form-control-static">{{ $profile->building_name }}</p>
-        </div>
-        <div class="subtotal">
-          <h3>小計</h3>
-          <p>商品代金: {{ $item->formatted_price }}</p>
-          <p>支払い方法: <span id="selected-payment-method">選択してください</span></p>
+      <div class="purchase-column">
+        <div class="subtotal-box">
+          <div class="subtotal-item-box">
+            <p class="subtotal-item"><span>商品代金</span><span>{{ $item->formatted_price }}</span></p>
+          </div>
+          <div class="subtotal-item-box">
+            <p class="subtotal-item"><span>支払い方法</span><span id="selected-payment-method">選択してください</span></p>
+          </div>
         </div>
         <button type="submit" class="btn btn-primary">購入する</button>
-      </form>
+      </div>
+    </div>
+    <hr class="section-divider">
+    <div class="purchase-form">
+      <div class="purchase-columns">
+        <div class="purchase-column">
+          <div class="form-group">
+            <label for="payment_method">支払い方法</label>
+            <select name="payment_method" class="form-control" required>
+                <option value="" disabled selected>選択してください</option>
+                <option value="コンビニ支払い">コンビニ支払い</option>
+                <option value="カード支払い">カード支払い</option>
+            </select>
+          </div>
+          <hr class="section-divider"> 
+          <div class="purchase-column delivery-address">
+            <div class="address-header">
+              <h3>配送先</h3>
+              <a href="/purchase/{{ $item->id }}/change-address" class="btn btn-secondary">変更する</a>
+            </div>
+            <div class="form-group">
+              <p class="form-control-static">〒{{ $profile->postal_code }}</p>
+            </div>
+            <div class="form-group">
+              <p class="form-control-static">{{ $profile->address }}</p>
+            </div>
+            <div class="form-group">
+              <p class="form-control-static">{{ $profile->building_name }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </main>
